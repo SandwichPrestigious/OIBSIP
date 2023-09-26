a1f631da162af2819daabe6446ae39d337008d0e
Swaroop Deshpande <swaroop.deshpande.20210101030@siics.org>
2023-09-26 14:53:13 +0530

    did somethings
    Signed-off-by: Swaroop Deshpande <swaroop.deshpande.20210101030@siics.org>

--- Task-1/LoginForm.java
+++ Task-1/LoginForm.java
@@ -1,27 +1,29 @@
+//packages imported for the form
 import javax.swing.*;
-import java.awt.*;
+import java.awt.*;//for gui
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+//creating a class for the form
 public class LoginForm extends JFrame {
-    private JTextField usernameField;
+        private JTextField usernameField;
     private JPasswordField passwordField;
 
     public LoginForm() {
-        setTitle("Login Form");
+        setTitle("LoginForm");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 150);
+        setSize(300, 150);// basic properties of the form
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 2));
+        panel.setLayout(new GridLayout(3, 2));//creating a panel for the form
 
         JLabel usernameLabel = new JLabel("Username:");
-        JLabel passwordLabel = new JLabel("Password:");
+        JLabel passwordLabel = new JLabel("Password:");//the labels for the form.
 
         usernameField = new JTextField(20);
-        passwordField = new JPasswordField(20);
+        passwordField = new JPasswordField(20);//the username and passwords are in the form of textfields.
 
-        JButton loginButton = new JButton("Login");
+        JButton loginButton = new JButton("Login");//creating the logun button
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -37,7 +39,7 @@ public class LoginForm extends JFrame {
         panel.add(loginButton);
 
         add(panel);
-        setVisible(true);
+        setVisible(true);// adding all panels to the form.
     }
 
     public static void main(String[] args) {
